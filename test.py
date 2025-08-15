25bc6995677af5e7b0787b87d3d002297785ccc5
Lohith Ishan Balasubramaniyan <lohith.ishan_2025@nyjc.edu.sg>
2025-08-15 04:24:21 +0000

    To dev we go. First iteration of "automated" testing

--- test.py
+++ test.py
@@ -1,16 +1,17 @@
 import unittest
-from entities.entity import Player, Enemy
+from entities.player import Player
+from entities.monster import Enemy
 
 class TestEntities(unittest.TestCase):
 
     def test_player(self):
-        player = Player(100, 100, "John", "He is a guy")
-        #self.assertIsInstance(player.calculate_atk(), int)
+        player = Player(100, "John", "He is a guy")
         print(player.display_stats())
+        self.assertIsInstance(player.calculate_atk(), int)
         
     def test_enemy(self):
-        enemy = Enemy(100, 100, "Jack", "He is a horrible guy", "boss", "roar")
-        print(enemy.enemy_type, enemy.atk_type)
+        enemy = Enemy(100, 100, "Jack", "He is a horrible guy", "boss")
+        print(enemy.enemy_type)
         
 
 if __name__ == '__main__':
