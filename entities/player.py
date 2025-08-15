c108d9863f153caf2d6973098d01e5264cb3114e
Saltensity <matthewna25@gmail.com>
2025-08-15 03:36:00 +0000

    Major update: entities and object update

---
+++ entities/player.py
@@ -0,0 +1,46 @@
+from random import random
+from entities.entity import Entity
+from objects.item import Item
+
+ARMOR = 'armor'
+WEAPON = 'weapon'
+
+class Player(Entity):
+    """The Player character. Several methods to manipulate inventory and weapon."""
+    def __init__(self, hp, name, description):
+        super().__init__(hp, None, name, description)
+        self.inventory = []
+        self.cur_weapon = None
+        self.cur_armor = None
+    
+    def open_inventory(self):
+        """Prints out the current items stored in a player's inventory."""
+        print("Your current items:", " ".join(self.inventory))
+        return
+    
+    def update_inventory(self, item):
+        """Updates the current inventory. RIGHT NOW, ONLY ADDS ITEM."""
+        self.inventory.append(item)
+        return
+    
+    def calculate_atk(self) -> int:
+        """Calculates the attack that this Player should deal."""
+        return round((1+random()) * self.cur_weapon.atk) # WEAPON ATTACK HERE
+        
+    
+    def calculate_dmg(self, base_dmg):
+        """Calculates total damage based on base damage and armor reduction."""
+        if not self.cur_armor:
+            self.hp -= base_dmg
+        else:
+            self.hp -= max(0, base_dmg - self.cur_armor)
+        
+        return
+    
+    def update_equipment(self, type, item: Item):
+        """Updates the current weapon. ```type``` can either be player.ARMOR or player.WEAPON"""
+        if type == ARMOR:
+            self.cur_armor = item
+        elif type == WEAPON:
+            self.cur_weapon = item
+        return
