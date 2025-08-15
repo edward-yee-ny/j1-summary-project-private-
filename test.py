d14f005d777a93c758a6d196c81328173f0a828f
Lohith Ishan Balasubramaniyan <lohith.ishan_2025@nyjc.edu.sg>
2025-08-15 04:03:03 +0000

    Testing Testing

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
+import unittest
+from entities.entity import Player, Enemy
 
+class TestEntities(unittest.TestCase):
+
+    def test_player(self):
+        player = Player(100, 100, "John", "He is a guy")
+        #self.assertIsInstance(player.calculate_atk(), int)
+        print(player.display_stats())
+        
+    def test_enemy(self):
+        enemy = Enemy(100, 100, "Jack", "He is a horrible guy", "boss", "roar")
+        print(enemy.enemy_type, enemy.atk_type)
+        
+
+if __name__ == '__main__':
+    unittest.main()
