73cee83398a7de7101f393eea1539b6badce8b4e
dooweidde <edward.yee.mynn.sherng_2025@nyjc.edu.sg>
2025-08-18 11:47:57 +0000

    small addition of room_type to help the main game loop

--- environment/boss.py
+++ environment/boss.py
@@ -3,6 +3,7 @@ from .battle import BattleRoom
 class BossRoom(BattleRoom):
     def __init__(self):
         super().__init__()
+        self.room_type = "boss"
         self.description = "You are in the boss room. Defeat the boss to win the game!"
         self.battle_type = 'boss'
         
--- environment/treasure.py
+++ environment/treasure.py
@@ -4,6 +4,7 @@ class TreasureRoom(Room):
     def __init__(self):
         super().__init__()
         self.item = None
+        self.room_type = "treasure"
         self.description = "You are in a treasure room."
         
     def get_reward(self):
