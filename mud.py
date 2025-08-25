c8956640a3b35b064f181109f3c4b11ae559e606
dooweidde <edward.yee.mynn.sherng_2025@nyjc.edu.sg>
2025-08-26 00:14:17 +0800

    Update mud.py

--- mud.py
+++ mud.py
@@ -1,3 +1,21 @@
+# importing
+    from environment.map import *
+    from objects.item import Item 
+    from entities.player import Player
+    from environment.boss import BossRoom
+    from helper.extract_json import extractJson
+    from time import sleep
+    from pack_json import 
+    import json
+
+    def open_and_dict(filename):
+        with open(filename, 'r') as f:
+            data = f.read()
+        res = extractJson(data)
+        return res
+
+    BATTLE_OUTCOMES = open_and_dict('data/battle_outcome.json')
+    DAMAGE_REPORT = open_and_dict('data/damage_report.json')
 class Game:
     def __init__(self):
         self.choice = input("""
@@ -6,171 +24,151 @@ class Game:
                 2. Load Game
                 Key in the number :
         """)
-    if choice == "1":
-        # importing
-        from environment.map import *
-        from objects.item import Item 
-        from entities.player import Player
-        from environment.boss import BossRoom
-        from helper.extract_json import extractJson
-        from time import sleep
 
-        def open_and_dict(filename):
-            with open(filename, 'r') as f:
-                data = f.read()
-            res = extractJson(data)
-            return res
+    # add in the basic options, want to start the game or not (strat game load game quit)
+    game = Map()
+    player = Player()
+    game_over = False
 
-        BATTLE_OUTCOMES = open_and_dict('data/battle_outcome.json')
-        DAMAGE_REPORT = open_and_dict('data/damage_report.json')
+    print("""
+    ___                                ___     _    _                                               _   _                   _      _   
+    |  _`\\  _                         /'___)   ( )_ ( )              /'\_/`\                        ( ) ( )        _        ( )    ( )_ 
+    | (_) )(_)  ___    __        _   | (__     | ,_)| |__     __     |     | \ ___     __     _ _   | |/'/'  ___  (_)   __  | |__  | ,_)
+    | ,  / | |/',__) /'__`\    /'_`\ | ,__)    | |  |  _ `\ /'__`\   | (_) | /'__`\ /'_ `\ /'_` )   | , <  /' _ `\| | /'_ `\|  _ `\| |  
+    | |\ \ | |\__, \(  ___/   ( (_) )| |       | |_ | | | |(  ___/   | | | |(  ___/( (_) |( (_| |   | |\`\ | ( ) || |( (_) || | | || |_ 
+    (_) (_)(_)(____/`\____)   `\___/'(_)       `\__)(_) (_)`\____)   (_) (_)`\____)`\__  |`\__,_)   (_) (_)(_) (_)(_)`\__  |(_) (_)`\__)
+                                                                                ( )_) |                           ( )_) |            
+                                                                                    \___/'                            \___/'            
+    """)
+    # functions used
+    def prompt_room_choice():
+        """
+        handles the room choice and moves into the room selected
+        handles the choice to save game data
+        """
+        while True:
+            choice = input("Key in your choice of 1,2,3,4 or 5: ")
+            print("\n")
+            if choice == "1":
+                game.go_left()
+                break
+            elif choice == "2":
+                game.go_forward()
+                break
+            elif choice == "3":
+                game.go_right()
+                break
+            elif choice == "4":
+                # allows the player ot equip the equipment of their choice
+                print("hello")
+            elif choice == "5":
+                file_path = "save_state.json"
+                with open(file_path, "w") as f{
+                    json.dump(game.direction_save_arr)
+                }
+                break
+            else:
+                print("\n Invalid input \n")
+        return None
 
-            # initialising
-        # add in the basic options, want to start the game or not (strat game load game quit)
-        game = Map()
-        player = Player()
+    def fight(enemy):
+        player.reset_hp()
         game_over = False
 
-        print("""
-        ___                                ___     _    _                                               _   _                   _      _   
-        |  _`\\  _                         /'___)   ( )_ ( )              /'\_/`\                        ( ) ( )        _        ( )    ( )_ 
-        | (_) )(_)  ___    __        _   | (__     | ,_)| |__     __     |     | \ ___     __     _ _   | |/'/'  ___  (_)   __  | |__  | ,_)
-        | ,  / | |/',__) /'__`\    /'_`\ | ,__)    | |  |  _ `\ /'__`\   | (_) | /'__`\ /'_ `\ /'_` )   | , <  /' _ `\| | /'_ `\|  _ `\| |  
-        | |\ \ | |\__, \(  ___/   ( (_) )| |       | |_ | | | |(  ___/   | | | |(  ___/( (_) |( (_| |   | |\`\ | ( ) || |( (_) || | | || |_ 
-        (_) (_)(_)(____/`\____)   `\___/'(_)       `\__)(_) (_)`\____)   (_) (_)`\____)`\__  |`\__,_)   (_) (_)(_) (_)(_)`\__  |(_) (_)`\__)
-                                                                                    ( )_) |                           ( )_) |            
-                                                                                        \___/'                            \___/'            
-        """)
-        # functions used
-        def prompt_room_choice():
-            """
-            handles the room choice and moves into the room selected
-            handles the choice to save game data
-            """
-            while True:
-                choice = input("Key in your choice of 1,2 or 3: ")
-                print("\n")
-                if choice == "1":
-                    game.go_left()
-                    break
-                elif choice == "2":
-                    game.go_forward()
-                    break
-                elif choice == "3":
-                    game.go_right()
-                    break
-                elif choice == "4":
-                    # allows the player ot equip the equipment of their choice
-                    print("hello")
-                elif choice == "5":
-                    #put save function here
-                    print("work in progress...")
-                    break
-                else:
-                    print("Invalid input \n")
-            return choice
+        # fighting loop
+        while not game_over:
+            
+            player.display_stats()
+            enemy.display_stats()
+            
+            sleep(2)
 
-        def fight(enemy):
-            player.reset_hp()
-            game_over = False
+            action = input("\n what action do you take \n 1. attack \n 2. open inventory \n input your choice: ")
+            
+            if action == "1": # player attack the enemy
+                multi, damage = player.calculate_atk()
+                enemy.update_hp(-damage)
+                print(DAMAGE_REPORT['Damage_Report']['Description'].replace("(multiplier)", str(multi)))
+                print(DAMAGE_REPORT['Damage_Report']['Damage_Dealt'].replace("(dmg)", str(damage)))
+                sleep(0.6)
 
-            # fighting loop
-            while not game_over:
+                if enemy.hp <= 0:
+                    print("\n", BATTLE_OUTCOMES['Victory'])
+                    # gives and autoequips the reward item for the player
+                    reward = game.currentRoom.reward
+                    player.update_inventory(reward)
+                    player.update_equipment(reward)
+                    break
+                # enemy attack the player
+                if player.calculate_dmg(enemy.atk):
+                    print("\n", BATTLE_OUTCOMES["Defeat"], '\n', BATTLE_OUTCOMES["Consolation"])
+                    exit(1)
                 
-                player.display_stats()
-                enemy.display_stats()
+                print(DAMAGE_REPORT['Opponent_Action']['Description'].replace("(dmg)", str(enemy.atk)))
 
-                action = input("\n what action do you take \n 1. attack \n 2. open inventory \n input your choice: ")
-                
-                if action == "1": # player attack the enemy
-                    multi, damage = player.calculate_atk()
-                    enemy.update_hp(-damage)
-                    print(DAMAGE_REPORT['Damage_Report']['Description'].replace("(multiplier)", str(multi)))
-                    print(DAMAGE_REPORT['Damage_Report']['Damage_Dealt'].replace("(dmg)", str(damage)))
-
-                    if enemy.hp <= 0:
-                        print("\n", "congrats you win")
-                        # gives and autoequips the reward item for the player
-                        reward = game.currentRoom.reward
-                        player.update_inventory(reward)
-                        player.update_equipment(reward)
-                        break
-                    # enemy attack the player
-                    if player.calculate_dmg(enemy.atk):
-                        print("\n", "Game over MegaKnight... Maybe try again next time!")
-                        game_over = BATTLE_OUTCOMES["Defeat"], '\n', BATTLE_OUTCOMES["Consolation"]
-                        exit(1)
-                elif action == "2":
-                    player.        
-                    while True:
-                        exit_ = input("\n exit? \n key in y to exit: ")
-                        if exit_ == "y":
-                            break
-                else:
-                    print("MEGAKNIGHT CANNOT FOLLOW YOUR COMMAND.")
-                
-                sleep(1)
-
-        def battle_sequence(battle_type):
-            # initialising necessary variables
-            if battle_type == "normal":
-                enemy = game.currentRoom.enemy
-                fight(enemy)
-            elif battle_type == "elite":
-                enemy1 = game.currentRoom.enemy
-                enemy2 = game.currentRoom.enemy2
-                fight(enemy1)
-                fight(enemy2)
-            else:
-                boss = game.currentRoom.enemy
-                fight(boss)
-
-            return None
-
-        def get_room_type():
-            """returns the type of room player currently is in"""
-            return game.currentRoom.room_type
-
-        def handle_room_action(room_type):
-            """depending on the type of room carries out the appropriate actions"""
-
-            if room_type == "treasure":
-                reward = game.currentRoom.item
-                player.update_inventory(reward)
-                print("\n", "A new item has been added into your inventory!")
-                player.update_equipment(reward)
+            elif action == "2":
                 player.open_inventory()
             else:
-                battle_sequence(room_type)
-
-
-        # intro
-        print("Welcome to the arena.",'\n', "You will assist Mega knight in competing in the tournament", '\n', "Winning each battle will give you an opportunity to choose your next opponent.", '\n', "Best of luck player")
-
-        # initial description of the starting room
-        game.displayCurrentRoom()
-
-        #game running code
-        while not game_over:
-            # display possible paths
-            print("Pick a direction to move in \n 1. Left \n 2. Middle \n 3. Right \n 4. Equip equipment \n 5. Save")
-        
-            # get the choice and move into the chosen room
-            choice = prompt_room_choice()
-
-            # check room type, handle room actions
-            if not game.currentRoom.isCompleted:
-                room_type = get_room_type()
-                handle_room_action(room_type)
-                game.currentRoom.isCompleted = True
+                print("MEGAKNIGHT CANNOT FOLLOW YOUR COMMAND.")
             
-            # If it's a BossRoom completed, end the game.
-            if isinstance(game.currentRoom, BossRoom):
-                game_over = True
+            sleep(1)
 
-        print("You have completed the dungeon!! :)","\n","We hope you come back again...")
-    elif choice == "2":
+    def battle_sequence(battle_type):
+        # initialising necessary variables
+        if battle_type == "normal":
+            enemy = game.currentRoom.enemy
+            fight(enemy)
+        elif battle_type == "elite":
+            enemy1 = game.currentRoom.enemy
+            enemy2 = game.currentRoom.enemy2
+            fight(enemy1)
+            fight(enemy2)
+        else:
+            boss = game.currentRoom.enemy
+            fight(boss)
+
+        return None
+
+    def get_room_type():
+        """returns the type of room player currently is in"""
+        return game.currentRoom.room_type
+
+    def handle_room_action(room_type):
+        """depending on the type of room carries out the appropriate actions"""
+
+        if room_type == "treasure":
+            reward = game.currentRoom.item
+            player.update_inventory(reward)
+            print("\n", "A new item has been added into your inventory!")
+            player.update_equipment(reward)
+            player.open_inventory()
+        else:
+            battle_sequence(room_type)
+
+
+    # intro
+    print("Welcome to the arena.",'\n', "You will assist Mega knight in competing in the tournament", '\n', "Winning each battle will give you an opportunity to choose your next opponent.", '\n', "Best of luck player")
+
+    # initial description of the starting room
+    game.displayCurrentRoom()
+
+    #game running code
+    while not game_over:
+        # display possible paths
+        print("Pick a direction to move in \n 1. Left \n 2. Middle \n 3. Right \n 4. Equip equipment \n 5. Save")
         
-    # save, load, start new game
-    # to load game or save game, have a separate file to save and load from
-    # 
+        # get the choice and move into the chosen room
+        prompt_room_choice()
+
+        # check room type, handle room actions
+        if not game.currentRoom.isCompleted:
+            room_type = get_room_type()
+            handle_room_action(room_type)
+            game.currentRoom.isCompleted = True
+        
+        # If it's a BossRoom completed, end the game.
+        if isinstance(game.currentRoom, BossRoom):
+            game_over = True
+
+    print("\n You have completed the dungeon!! :)","\n","We hope you come back again...")
     pass
