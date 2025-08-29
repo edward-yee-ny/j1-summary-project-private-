41b7d846fba1642ce8ccedb473d9dc1ab996742b
Lohith Ishan Balasubramaniyan <lohith.ishan_2025@nyjc.edu.sg>
2025-08-29 03:16:50 +0000

    Faster speed

--- helper/typer.py
+++ helper/typer.py
@@ -1,15 +1,14 @@
 import time
 import sys
+import select 
 
-saved_lens = []
 # Function to create the typewriter effect
 def write_effect(sentence, i):
     # Loop through each character in the sentence
-    saved_lens.append(len(sentence))
     for char in sentence:
         sys.stdout.write(char)
         sys.stdout.flush()
-        time.sleep(0.03)
+        time.sleep(0.027)
     if i:
         u = input("")
         return u
@@ -24,9 +23,6 @@ typical settings.
 type_delay = 0.05
 delete_delay = 0.01
 
-write_effect("Hello!", False)
-write_effect("HIIII", True)
-
-delete_effect()
+write_effect("Hello!Hello!Hello!Hello!Hello!Hello!Hello!Hello!Hello!Hello!Hello!Hello!Hello!", False)
 write_effect("HIIII", False)
 """
